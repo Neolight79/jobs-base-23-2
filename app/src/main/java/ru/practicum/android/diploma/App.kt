d656c7a2a2c84f78048f34882ae53afab0696849
Sergey <133217436+Neolight79@users.noreply.github.com>
2025-08-30 03:19:06 +0300

    Создан файл класса приложения и прописан в AndroidManifest.xml

---
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -0,0 +1,12 @@
+package ru.practicum.android.diploma
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // ToDo реализовать операции, выполняющиеся при инициализации приложения
+    }
+
+}
