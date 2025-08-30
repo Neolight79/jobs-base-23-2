72adc06def08e0b6888fe1352b7849f0216cd37c
ElenaPupysheva <nyankem@gmail.com>
2025-08-30 14:00:54 +0300

    Исправления по detect

--- app/src/main/java/ru/practicum/android/diploma/ui/theme/Color.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Color.kt
@@ -2,14 +2,20 @@ package ru.practicum.android.diploma.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val LightGray = Color(0xFFE6E8EB)
-val Gray = Color(0xFFAEAFB4)
-val Blue = Color(0xFF3772E7)
+private const val COLOR_E6E8EB = 0xFFE6E8EB
+private const val COLOR_AEAFB4 = 0xFFAEAFB4
+private const val COLOR_3772E7 = 0xFF3772E7
+private const val COLOR_FDFDFD = 0xFFFDFDFD
+private const val COLOR_1A1B22 = 0xFF1A1B22
+
+val LightGray = Color(COLOR_E6E8EB)
+val Gray = Color(COLOR_AEAFB4)
+val Blue = Color(COLOR_3772E7)
 
 // Цвета LightMode
-val WhiteDay = Color(0xFFFDFDFD)
-val BlackDay = Color(0xFF1A1B22)
+val WhiteDay = Color(COLOR_FDFDFD)
+val BlackDay = Color(COLOR_1A1B22)
 
 // Цвета DarkMode
-val WhiteNight = Color(0xFF1A1B22)
-val BlackNight = Color(0xFFFDFDFD)
+val WhiteNight = Color(COLOR_1A1B22)
+val BlackNight = Color(COLOR_FDFDFD)
