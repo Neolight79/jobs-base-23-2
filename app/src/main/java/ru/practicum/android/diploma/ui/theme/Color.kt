bf41b7d30f7492227de771a8616b8fc0eb3559e7
ElenaPupysheva <nyankem@gmail.com>
2025-08-30 12:06:44 +0300

    Создание файлов темы приложения и вспомогательных

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package ru.practicum.android.diploma.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightGray = Color(0xFFE6E8EB)
+val Gray = Color(0xFFAEAFB4)
+val Blue = Color(0xFF3772E7)
+
+//Цвета LightMode
+val WhiteDay = Color(0xFFFDFDFD)
+val BlackDay = Color(0xFF1A1B22)
+
+//Цвета DarkMode
+val WhiteNight = Color(0xFF1A1B22)
+val BlackNight = Color(0xFFFDFDFD)
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Dimens.kt
@@ -0,0 +1,14 @@
+package ru.practicum.android.diploma.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val FontSizeRegular = 16.sp
+val FontSizeLarge = 32.sp
+val FontSizeMedium = 22.sp
+val FontSizeSmall = 12.sp
+
+val LineHeightRegular = 19.dp
+val LineHeightLarge  = 38.dp
+val LineHeightMedium = 26.dp
+val LineHeightSmall = 16.dp
