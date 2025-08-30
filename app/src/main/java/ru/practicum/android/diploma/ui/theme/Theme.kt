321b27b797b088be6a89cf15aeb2c9ced944d954
ElenaPupysheva <nyankem@gmail.com>
2025-08-30 13:15:48 +0300

    Настройка Theme

--- app/src/main/java/ru/practicum/android/diploma/ui/theme/Theme.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Theme.kt
@@ -1,2 +1,35 @@
 package ru.practicum.android.diploma.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val DarkColorScheme = darkColorScheme(
+    primary = WhiteNight,
+    secondary = BlackNight,
+    tertiary = Gray
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = WhiteDay,
+    secondary = BlackDay,
+    tertiary = LightGray
+)
+
+@Composable
+fun DiplomaAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+){
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
