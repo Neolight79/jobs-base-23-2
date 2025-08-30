643baa61e7432c557ac61edbe94fb21ce79aecc2
owenear <owenear@gmail.com>
2025-08-31 00:27:44 +0300

    Созданы интерфейсы и классы для работы с базой данных избранных вакансий Room

---
+++ app/src/main/java/ru/practicum/android/diploma/data/AppDatabase.kt
@@ -0,0 +1,13 @@
+package ru.practicum.android.diploma.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import ru.practicum.android.diploma.data.dto.VacancyFavoriteEntity
+
+
+@Database(version = 1, entities = [VacancyFavoriteEntity::class])
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun favoriteDao(): FavoriteDao
+
+}
