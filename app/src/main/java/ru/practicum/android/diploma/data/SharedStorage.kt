95fc0614211901462e7433bbdd05ad54dad2daa3
ElenaPupysheva <nyankem@gmail.com>
2025-09-10 14:25:06 +0300

    Исправление по detekt

--- app/src/main/java/ru/practicum/android/diploma/data/SharedStorage.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/SharedStorage.kt
@@ -1,6 +1,7 @@
 package ru.practicum.android.diploma.data
 
 import ru.practicum.android.diploma.data.dto.FilterParametersDto
+
 interface SharedStorage {
     fun putData(data: FilterParametersDto)
     fun getData(defaultData: FilterParametersDto): FilterParametersDto
